15c7dcbdc3053ed4d9d7befd0f77fa8af828af44
Rocio Batres <jonasgirl21@gmail.com>
2020-09-17 06:13:28 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -7,7 +7,6 @@ def line(katz_deli)
   if katz_deli.length == 0 
     puts "The line is currently empty." 
   else   
-    katz_deli.length >= 1
     katz_deli.each do |customer|
     line.push("#. #") 
     position += 1 
