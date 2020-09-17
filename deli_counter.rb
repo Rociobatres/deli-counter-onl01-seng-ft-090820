d49c8fb4c61c6b824a91f6ced64686eb2ab8ff77
Rocio Batres <jonasgirl21@gmail.com>
2020-09-17 06:00:48 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -5,15 +5,15 @@ katz_deli = []
 def line(katz_deli)
   position = 1
   line = []
-  if katz_deli.length == 0 
+    if katz_deli.length == 0 
     puts "The line is currently empty." 
-  else   
+    else   
     katz_deli.length >= 1
     katz_deli.each do |customer|
     line.push("#. #") 
     position += 1 
     end   
-  end 
+  puts  
 end
 
 def take_a_number(katz_deli, name)
