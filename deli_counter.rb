11e2873a6b16ee73db41f2911acfd678c5e64f8d
Rocio Batres <jonasgirl21@gmail.com>
2020-09-17 06:02:18 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -5,15 +5,16 @@ katz_deli = []
 def line(katz_deli)
   position = 1
   line = []
-    if katz_deli.length == 0 
+  if katz_deli.length == 0 
     puts "The line is currently empty." 
-    else   
+  else   
     katz_deli.length >= 1
     katz_deli.each do |customer|
     line.push("#. #") 
     position += 1 
-    end   
-  puts "The line is currently: #{line.join(" ")}" 
+  end   
+  puts "The line is currently: #{line.join(" ")}"
+end 
 end
 
 def take_a_number(katz_deli, name)
