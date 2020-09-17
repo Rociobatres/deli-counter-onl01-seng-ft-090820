0c8eab9660796b9d033500b69946b6a6278a6c0a
Rocio Batres <jonasgirl21@gmail.com>
2020-09-17 06:03:55 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -19,7 +19,7 @@ end
 
 def take_a_number(katz_deli, name)
   katz_deli.push(name) 
-  puts "Welcome, #. You are number #. in line."
+  puts "Welcome, #. You are number # in line."
   end   
 end   
 
